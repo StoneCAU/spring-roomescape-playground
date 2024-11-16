ad8dac86803d98db494d73af43fbfd4fb4211d09
StoneCAU <seokwoo1999@naver.com>
2024-11-16 16:30:28 +0900

    feat: 각 객체의 id에 대한 setter 함수 추가

--- src/main/java/roomescape/domain/Reservation.java
+++ src/main/java/roomescape/domain/Reservation.java
@@ -4,14 +4,17 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class Reservation {
+    @Setter
     private Long id;
     private String name;
     private String date;
     private Time time;
+
 }
--- src/main/java/roomescape/domain/Time.java
+++ src/main/java/roomescape/domain/Time.java
@@ -4,12 +4,14 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class Time {
+    @Setter
     private Long id;
     private String time;
 }
