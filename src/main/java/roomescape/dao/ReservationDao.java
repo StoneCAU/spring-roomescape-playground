cb68ec9d4b6c42f45801eafe7bf55f30a5394735
StoneCAU <seokwoo1999@naver.com>
2024-11-16 16:29:54 +0900

    fix: 각 객체 간 id가 부여되지 않는 오류 수정

--- src/main/java/roomescape/dao/ReservationDao.java
+++ src/main/java/roomescape/dao/ReservationDao.java
@@ -4,6 +4,8 @@ import java.sql.PreparedStatement;
 import java.sql.Statement;
 import lombok.RequiredArgsConstructor;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
 import roomescape.domain.Reservation;
 
@@ -14,15 +16,19 @@ public class ReservationDao {
     private final JdbcTemplate jdbcTemplate;
 
     public void insert(Reservation reservation) {
-
         String sql = "INSERT INTO reservation (name, date, time_id) VALUES (?, ?, ?)";
 
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+
         jdbcTemplate.update(connection -> {
-            PreparedStatement ps = connection.prepareStatement(sql, Statement.NO_GENERATED_KEYS);
+            PreparedStatement ps = connection.prepareStatement(sql, new String[]{"id"});
             ps.setString(1, reservation.getName());
             ps.setString(2, reservation.getDate());
             ps.setLong(3, reservation.getTime().getId());
             return ps;
-        });
+        }, keyHolder);
+
+        Long id = keyHolder.getKey().longValue();
+        reservation.setId(id);
     }
 }
--- src/main/java/roomescape/dao/TimeDao.java
+++ src/main/java/roomescape/dao/TimeDao.java
@@ -1,10 +1,11 @@
 package roomescape.dao;
 
 import java.sql.PreparedStatement;
-import java.sql.Statement;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
 import roomescape.domain.Time;
 
@@ -14,22 +15,33 @@ public class TimeDao {
 
     private final JdbcTemplate jdbcTemplate;
 
+    public void insert(Time time) {
+        String sql = "insert into time (time) values ?";
+
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+
+        jdbcTemplate.update(connection -> {
+            PreparedStatement ps = connection.prepareStatement(sql, new String[]{"id"});
+            ps.setString(1, time.getTime());
+            return ps;
+        }, keyHolder);
+
+        Long id = keyHolder.getKey().longValue();
+        time.setId(id);
+    }
+
+    public void delete(Long timeId) {
+        String sql = "delete from time where id = ?";
+
+        jdbcTemplate.update(sql, timeId);
+    }
+
     public Time findById(Long timeId) {
         String sql = "select * from time where id = ?";
 
         return jdbcTemplate.query(sql,
                 (rs,rowNum) -> new Time(rs.getLong("id"), rs.getString("time")), timeId)
                 .stream().findFirst().orElse(null);
-    }
-
-    public void insert(Time time) {
-        String sql = "insert into time (time) values ?";
-
-        jdbcTemplate.update(connection -> {
-            PreparedStatement ps = connection.prepareStatement(sql, Statement.NO_GENERATED_KEYS);
-            ps.setString(1, time.getTime());
-            return ps;
-        });
     }
 
     public List<Time> findAll() {
